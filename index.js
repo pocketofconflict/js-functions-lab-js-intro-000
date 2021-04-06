a0be16ca55141c485cd58320c8aa180d897ddea8
Christopher Diaz <pocketofconflict@gmail.com>
2021-04-06 21:05:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays(string) {
   return ("Happy holidays!")
 }
 function happyHolidaysTo(string) {
-  var name = "Chris"
+  var name = "you"
   return (`Happy holidays, ${name}!`)
 }
