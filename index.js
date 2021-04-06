d7e838404cee520258c1fc9086a7014377912051
Christopher Diaz <pocketofconflict@gmail.com>
2021-04-06 21:17:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays(string) {
   return ("Happy holidays!")
 }
 function happyHolidaysTo(string) {
-  var name = "you", "everyone"
+  var name = ("you", "everyone")
   return (`Happy holidays, ${name}!`)
 }
